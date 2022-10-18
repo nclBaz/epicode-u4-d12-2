fa75ab4da560e91e4d5c2e9e0e2da8f6f94585e8
Riccardo Gulin <riccardo@nucleode.com>
2022-10-18 10:52:21 +0200

    Adds POST & GET /users

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import listEndpoints from "express-list-endpoints"
 import cors from "cors"
 import mongoose from "mongoose"
+import usersRouter from "./api/users/index.js"
 
 const server = express()
 const port = process.env.PORT || 3001
@@ -11,6 +12,7 @@ server.use(cors())
 server.use(express.json())
 
 // *************************************** ENDPOINTS ***************************************************
+server.use("/users", usersRouter)
 
 // ************************************ ERROR HANDLERS *************************************************
 
